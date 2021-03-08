46c5bd78557b75ee5c59c22b5f49d5c97f7fb536
visheshdvn <vishesh.dh1@gmail.com>
2021-03-08 15:29:56 +0530

    font scss created

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,23 @@
+import React, {Component} from 'react'
 import '../styles/main.scss'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+class MyApp extends Component {
+
+  static async getInitialProps({Component, ctx}) {
+    let pageProps = {};
+
+    if (Component.getInitialProps) {
+      pageProps = await Component.getInitialProps(ctx);
+    }
+
+    return { pageProps }
+  }
+
+  render() {
+    const { Component, pageProps } = this.props;
+    return <Component {...pageProps} />
+  }
 }
 
 export default MyApp
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react'
 import BaseLayout from '../components/layouts/BaseLayout'
+import {Button, Container} from 'reactstrap'
 
 export class Index extends Component {
   render() {
     return (
       <BaseLayout>
+      <Container>
         <h1>I am Index Page.</h1>
+        <Button color="danger">Danger!</Button>
+      </Container>
       </BaseLayout>
     )
   }
